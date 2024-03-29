b150a7b0130bc6a0526f94745f555c313ac551a8
fred <domaine75@gmail.com>
2024-03-29 11:54:57 +0100

    Ajoutez des fichiers projet.

---
+++ WindowsFormsAppDemoPleaseWait/FormMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsAppDemoPleaseWait
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.buttonStart = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // buttonStart
+      // 
+      this.buttonStart.Location = new System.Drawing.Point(118, 136);
+      this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.buttonStart.Name = "buttonStart";
+      this.buttonStart.Size = new System.Drawing.Size(218, 36);
+      this.buttonStart.TabIndex = 0;
+      this.buttonStart.Text = "Start a long process";
+      this.buttonStart.UseVisualStyleBackColor = true;
+      this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1200, 703);
+      this.Controls.Add(this.buttonStart);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.Name = "FormMain";
+      this.Text = "WinForm application demo for please wait window";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button buttonStart;
+  }
+}
+
---
+++ WindowsFormsAppDemoPleaseWait/WaitWindow.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsAppDemoPleaseWait
+{
+  partial class WaitWindow
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.labelWait = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // labelWait
+      // 
+      this.labelWait.AutoSize = true;
+      this.labelWait.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.labelWait.Location = new System.Drawing.Point(55, 52);
+      this.labelWait.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.labelWait.Name = "labelWait";
+      this.labelWait.Size = new System.Drawing.Size(980, 31);
+      this.labelWait.TabIndex = 0;
+      this.labelWait.Text = "Please wait. This window will close automatically at the end of the process";
+      // 
+      // WaitWindow
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1067, 150);
+      this.Controls.Add(this.labelWait);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.Name = "WaitWindow";
+      this.Text = "WaitWindow";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label labelWait;
+  }
+}
